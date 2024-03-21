2b6b0aafbb0e2aa53cd06d209649347e92fd7354
MarthTheKing <marththeking1501@gmail.com>
2024-03-21 15:48:53 -0600

    fixed route missing from merge

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<CocktailHome/>} />
+          <Route path='/non-alcoholic' element={<CocktailHome/>} />
           <Route path='/id/:idnumber' element={<CocktailDetails/>} />
           {/* <Route path='/search/results' element={<CocktailSearch/>} /> */}
         </Routes>
